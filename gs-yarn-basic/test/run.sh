3bbeeafb20694e2d6bad2d8267c061110d41cfbc
Janne Valkealahti <janne.valkealahti@gmail.com>
2014-02-19 11:41:26 +0000

    updates

--- gs-yarn-basic/test/run.sh
+++ gs-yarn-basic/test/run.sh
@@ -3,34 +3,18 @@ cd $(dirname $0)
 
 cd ../complete
 
-mvn clean package
+./gradlew clean build
 ret=$?
 if [ $ret -ne 0 ]; then
   exit $ret
 fi
-rm -rf target
-
-./gradlew build
-ret=$?
-if [ $ret -ne 0 ]; then
-  exit $ret
-fi
-rm -rf build
 
 cd ../initial
 
-mvn clean package
+./gradlew clean
 ret=$?
 if [ $ret -ne 0 ]; then
   exit $ret
 fi
-rm -rf target
-
-./gradlew build
-ret=$?
-if [ $ret -ne 0 ]; then
-  exit $ret
-fi
-rm -rf build
 
 exit
