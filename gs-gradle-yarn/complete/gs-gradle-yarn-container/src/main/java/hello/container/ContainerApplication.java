d1de5d760c987783bc2ff2e563e59a345540ad44
Janne Valkealahti <janne.valkealahti@gmail.com>
2014-03-26 09:15:27 +0000

    updates

--- gs-gradle-yarn/complete/gs-gradle-yarn-container/src/main/java/hello/container/ContainerApplication.java
+++ gs-gradle-yarn/complete/gs-gradle-yarn-container/src/main/java/hello/container/ContainerApplication.java
@@ -2,9 +2,7 @@ package hello.container;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.context.annotation.Configuration;
 
-@Configuration
 @EnableAutoConfiguration
 public class ContainerApplication {
 
