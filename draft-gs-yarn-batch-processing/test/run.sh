507c4f266521c5e3783d072a9859246ea794a216
Janne Valkealahti <janne.valkealahti@gmail.com>
2014-04-03 15:26:20 +0100

    updates

---
+++ draft-gs-yarn-batch-processing/test/run.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+cd $(dirname $0)
+
+cd ../complete
+./gradlew clean build
+ret=$?
+if [ $ret -ne 0 ]; then
+  exit $ret
+fi
+
+./gradlew clean
+ret=$?
+if [ $ret -ne 0 ]; then
+  exit $ret
+fi
+
+mvn clean package
+ret=$?
+if [ $ret -ne 0 ]; then
+  exit $ret
+fi
+
+mvn clean
+ret=$?
+if [ $ret -ne 0 ]; then
+  exit $ret
+fi
+
+exit
